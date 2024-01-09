ab1d84722a70e223dbbcbe5825782db6ee71fa7e
brookstellekamp <62809069+brookstellekamp@users.noreply.github.com>
2024-01-09 11:05:06 -0700

    dataAnalysis name

--- uv_vis.py
+++ uv_vis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
-from brooks.plotter import axis_eV_nm
+from dataAnalysis.plotter import axis_eV_nm
 
 def getData(filename, sampleID, substrate = None):
     '''
